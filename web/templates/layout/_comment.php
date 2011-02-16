57521571b84fd458771215cf8b2437b52482073e
reeze <reeze.xia@gmail.com>
2011-02-16 19:22:01 +0800

    Remove disqus link

--- web/templates/layout/_comment.php
+++ web/templates/layout/_comment.php
@@ -21,4 +21,3 @@
     })();
 </script>
 <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-<a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
