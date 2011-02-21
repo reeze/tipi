e028f52c9e20aed37447967062ea2522b38aae3f
reeze <reeze.xia@gmail.com>
2011-02-21 22:28:41 +0800

    Add image src resovle, now images' url are related to markdown files

--- web/lib/MarkdownExtraGeshiSyntax/markdown_geshi/markdown_geshi.php
+++ web/lib/MarkdownExtraGeshiSyntax/markdown_geshi/markdown_geshi.php
@@ -105,6 +105,69 @@ class MarkdownExtraGeshi_Parser extends MarkdownExtra_Parser
 		self::$quote_class = empty($matches[2]) ? '' : $matches[2];
 		return $matches[3];	
 	}
+
+	function _doImages_reference_callback($matches) {
+		$whole_match = $matches[1];
+		$alt_text    = $matches[2];
+		$link_id     = strtolower($matches[3]);
+
+		if ($link_id == "") {
+			$link_id = strtolower($alt_text); # for shortcut links like ![this][].
+		}
+
+		$alt_text = $this->encodeAttribute($alt_text);
+		if (isset($this->urls[$link_id])) {
+			// TIPI replacement
+			$_url = $this->urls[$link_id];
+			$url = $this->__tipi_web_image_repalce($_url);
+			$url = $this->encodeAttribute($url);
+
+			$result = "<img src=\"$url\" alt=\"$alt_text\"";
+			if (isset($this->titles[$link_id])) {
+				$title = $this->titles[$link_id];
+				$title = $this->encodeAttribute($title);
+				$result .=  " title=\"$title\"";
+			}
+			$result .= $this->empty_element_suffix;
+			$result = $this->hashPart($result);
+		}
+		else {
+			# If there's no such link ID, leave intact:
+			$result = $whole_match;
+		}
+
+		return $result;
+	}
+
+	function _doImages_inline_callback($matches) {
+		$whole_match	= $matches[1];
+		$alt_text		= $matches[2];
+		$url			= $matches[3] == '' ? $matches[4] : $matches[3];
+		$title			=& $matches[7];
+
+		// TIPI replacement
+		$url = $this->__tipi_web_image_repalce($url);
+
+		$alt_text = $this->encodeAttribute($alt_text);
+		$url = $this->encodeAttribute($url);
+		$result = "<img src=\"$url\" alt=\"$alt_text\"";
+		if (isset($title)) {
+			$title = $this->encodeAttribute($title);
+			$result .=  " title=\"$title\""; # $title already quoted
+		}
+		$result .= $this->empty_element_suffix;
+
+		return $this->hashPart($result);
+	}
+
+	function __tipi_web_image_repalce($url) {
+		// don't replace the absolute urls
+		if(strpos($url, ':') === false) {
+			return str_replace('images/', 'images/book/', $url);
+		}
+
+		return $url;
+	}
 }
 
 ?>
