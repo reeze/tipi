58df14118b172bacd6fc53bf5c67337f9acf3685
reeze <reeze.xia@gmail.com>
2011-02-26 02:36:35 +0800

    Revert missing tool tips

--- web/templates/layout/_book_tools.php
+++ web/templates/layout/_book_tools.php
@@ -16,14 +16,14 @@ $page_path_count = count($page_path);
 	</div>
 	<?php $headers = $page->getOutlineHeaders(); ?>
 	<div id="book_item_tools" class="fr" style="position: relative">
-		<a href='#comment' id="comment_link" class="tool_item" title="发表下看法吧!"><span>Comment</span></a>
+		<a href='#comment' id="comment_link" class="tool_item" title="有想法? 那就说点神马吧!"><span>Comment</span></a>
 		<a href='#' id="font_size_changer" class="tool_item"><span>Font Size</span></a>
 		<div id="font_size_pannel" class="dialog_box clearfix">
 			<a href='javascript:void(0);' id='font_size_decr' title="减小字体">-</a>
 			<a href='javascript:void(0);' id='font_size_default' title="恢复默认">1</a>
 			<a href='javascript:void(0);' id='font_size_incr' title="增大字体">+</a>
 		</div>
-		<a href='#' id="share_page" class="tool_item"><span>Share</span></a>
+		<a href='#' id="share_page" class="tool_item" title="觉得内容不错? 分享一下吧!"><span>Share</span></a>
 		<div id="share_page_pannel" class="dialog_box">
 			<div class="bottom_border">分享到:</div>
 			<?php echo get_jia_this($page->getAbsTitle()); ?>
