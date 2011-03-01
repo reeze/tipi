f0bce6183e3e53741e435a4bf876140bb2c3a269
reeze <reeze.xia@gmail.com>
2011-03-01 13:30:14 +0800

    fix lost sidebar

--- web/templates/layout/_book_navor.php
+++ web/templates/layout/_book_navor.php
@@ -1,11 +1,11 @@
 <div id="book_page_navor" class="clearfix">
-	<?php if($page && $page->getPrevPage()): ?>
+	<?php if($page && ($prev_page = $page->getPrevPage())): ?>
 	<div id="prev_page_link">
 		<a href="?p=<?php echo $prev_page->getPageName(); ?>">«&nbsp;<?php echo $prev_page->getTitle(); ?></a>	
 	</div>
 	<?php endif; ?>
 
-	<?php if($page && $page->getNextPage()): ?>
+	<?php if($page && ($next_page = $page->getNextPage())): ?>
 	<div id="next_page_link">
 		<a href="?p=<?php echo $next_page->getPageName(); ?>"><?php echo $next_page->getTitle(); ?>&nbsp;»</a>
 	</div>
