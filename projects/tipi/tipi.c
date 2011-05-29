478e258c7efeb157ff44bf63c89a896104b959af
martin <phppan.p@gmail.com>
2011-05-29 18:07:59 +0800

    批量替换的头部问题

--- projects/tipi/tipi.c
+++ projects/tipi/tipi.c
@@ -1,18 +1,18 @@
 /*
   +----------------------------------------------------------------------+
- \n PHP Version 5                                                       \n
+  | PHP Version 5                                                        |
   +----------------------------------------------------------------------+
- \n Copyright (c) 1997-2008 The PHP Group                               \n
+  | Copyright (c) 1997-2008 The PHP Group                                |
   +----------------------------------------------------------------------+
- \n This source file is subject to version 3.01 of the PHP license,     \n
- \n that is bundled with this package in the file LICENSE, and is       \n
- \n available through the world-wide-web at the following url:          \n
- \n http://www.php.net/license/3_01.txt                                 \n
- \n If you did not receive a copy of the PHP license and are unable to  \n
- \n obtain it through the world-wide-web, please send a note to         \n
- \n license@php.net so we can mail you a copy immediately.              \n
-  +----------------------------------------------------------------------+
- \n Author:                                                             \n
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  |----------------------------------------------------------------------+
+  | Author:                                                              |
   +----------------------------------------------------------------------+
 */
 
