d45ebc2291956168d112c62d72d9858cc945ba90
reeze <reeze.xia@gmail.com>
2011-06-01 00:47:35 +0800

    Add Release file

--- build/make_pdf.sh
+++ build/make_pdf.sh
@@ -2,7 +2,7 @@
 
 PDF_MAKER=wkhtmltopdf
 RELEASE_FILE_PATH=web/releases/`cat VERSION`.pdf
-BASE_URL=http://localhost/tipi/portable
+BASE_URL=http://www.php-internal.com/portable
 OPTIONS=" -L 0 -R 0 -T 9mm"
 
 $PDF_MAKER $OPTIONS --header-html $BASE_URL/header.php cover $BASE_URL/cover.php $BASE_URL/print.php $RELEASE_FILE_PATH
