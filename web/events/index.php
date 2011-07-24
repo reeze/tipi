c3c3f802800ffaf3ad40d61d3872f22625777db3
reeze <reeze.xia@gmail.com>
2011-07-24 20:47:24 +0800

    Revert event

--- web/events/index.php
+++ web/events/index.php
@@ -4,12 +4,7 @@ require_once "../lib/common.inc.php";
 require_once "../lib/SimpieView.php";
 
 $view = new SimpieView('../../HISTORY', "../templates/layout/common_page.php", null, SimpieView::MARKDOWN_VIEW);
-$html = $view->render(array(
+$view->render(array(
 	'current_page' => 'events',
 	'title' => 'TIPI大事记',
-), true);
-
-if(!file_exists('index.html')){
-	file_put_contents('index.html', $html);
-}
-echo $html;
+));
