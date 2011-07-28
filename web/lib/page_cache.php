973097870c5d6483fb6e419f55b03edcffd72a99
reeze <reeze.xia@gmail.com>
2011-07-28 23:23:39 +0800

    Add conditional page cache

--- web/lib/page_cache.php
+++ web/lib/page_cache.php
@@ -4,10 +4,18 @@ require_once dirname(__FILE__) . "/SimpieCache.php";
 require_once dirname(__FILE__) . "/TIPI.php";
 
 /**
- * 缓存页面
+ * 简单的页面缓存。通过URL+TIPI版本的方式来进行页面的缓存。
+ * 通过捕获output buffer来将输出内容进行缓存。目前页面均没有
+ * 会改变页面内容的行为。所以可以简单进行缓存.
+ *
+ * 缓存默认存放在/tmp/simpie-cache-*, 如果需要清空可以手动删除缓存
+ * 数据
  */
 
-if(!ENABLE_PAGE_CACHE) return;
+/**
+ * 禁用缓存或者是POST请求的时候将不会缓存
+ */
+if(!ENABLE_PAGE_CACHE || !empty($_POST)) return;
 
 $key = $_SERVER['REQUEST_URI'] . TIPI::getVersion();
 
