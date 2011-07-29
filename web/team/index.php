94830d4ca43e5745e9f9307a6bf962c5d7f4ff77
reeze <reeze.xia@gmail.com>
2011-07-29 11:49:42 +0800

    去掉手动缓存

--- web/team/index.php
+++ web/team/index.php
@@ -4,11 +4,7 @@ require_once "../lib/common.inc.php";
 require_once "../lib/SimpieView.php";
 
 $view = new SimpieView('../../AUTHORS', "../templates/layout/common_page.php", null, SimpieView::MARKDOWN_VIEW);
-$html = $view->render(array(
+$view->render(array(
 	'current_page' => 'team',
 	'title' => 'TIPI团队',
-),true);
-if(!file_exists('index.html')){
-	file_put_contents('index.html', $html);
-}
-echo $html;
+));
