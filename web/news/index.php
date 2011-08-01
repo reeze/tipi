97698f975f4d10f49cab2b6435785cf99f65fb10
reeze <reeze.xia@gmail.com>
2011-08-01 13:36:00 +0800

    调整News页面标题

--- web/news/index.php
+++ web/news/index.php
@@ -13,17 +13,20 @@ try
 	if(!$page_name) {
 		$view_path	= TEMPLATE_PATH . '/news/list.php';
 		$params 	= array(
-			'news_array' => News::findAll()
+			'news_array' => News::findAll(),
+			'title' => '新闻',
 		);
 	}
 	else {
 		$view_path	= NEWS_ROOT_PATH . "/{$page_name}." . MarkdownPage::extension;
+		$news_page = new News($page_name);
 		$params		= array(
-			'news' => new News($page_name)
+			'news' => $news_page,
+			'title' => $news_page->getTitle() . " - 新闻",
 		);
 	}
 
-	$params = array_merge($params, array('current_page' => 'news', 'title' => '新闻'));
+	$params = array_merge($params, array('current_page' => 'news'));
 
 	$view = new SimpieView($view_path, "../templates/layout/common_page.php");
 	$view->render($params);
