e55de2ca4695b8f68e09e5ea69abbbebd2302055
reeze <reeze.xia@gmail.com>
2012-02-09 15:47:51 +0800

    -　避免首页的base_url重复计算 -　增加部署的自定义端口

--- web/helpers/url_helper.php
+++ web/helpers/url_helper.php
@@ -29,12 +29,12 @@ function is_external_url($url)
  * 		url_for("/") 这返回项目的主目录一般为根域名, 而在开发时可能不是, 
  * 		例如链接到书籍首页的地址为: url_for("/book/?p=index")
  */
-function url_for($url, $absolute=false)
+function url_for($url, $absolute=true)
 {
 	if(is_external_url($url)) return $url;
 
 	static $base_url = null;
-	if(!$base_url) {
+	if($base_url === null) {
 		$backtrace = debug_backtrace();
 
 		$base_path 		= dirname(dirname(__FILE__));    		// 这个路径是项目的物理路径,
@@ -57,9 +57,12 @@ function url_for($url, $absolute=false)
 
 	$protocal = (isset($_SERVER['HTTP_PORT']) && $_SERVER['HTTP_PORT'] == '443') ? 'https' : 'http';
 
+	$port	  = (isset($_SERVER['HTTP_PORT']) && $_SERVER['HTTP_PORT']) ? $_SERVER['HTTP_PORT'] : "";
+	$port_str = ($port == '80' || $port == '443') ? "" : ":" . $port;
+
 	// !isset的情况下就是在命令行模式下的，这时使用默认的线上host信息
 	$host     = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : ONLINE_HOSTNAME;
 
-	return ($absolute ? "{$protocal}://{$host}" : "" ) . $base_url . $url;
+	return ($absolute ? "{$protocal}://{$host}{$port_str}" : "" ) . $base_url . $url;
 }
 
