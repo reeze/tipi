f7db42b66dd4f7e78ae006b1d6369989c1d36427
Reeze Xia <reeze@php.net>
2013-04-19 00:13:26 +0800

    Move ga to head section

--- web/templates/layout/_footer.php
+++ web/templates/layout/_footer.php
@@ -23,13 +23,3 @@
 		</div>
 	</div>
 </div>
-
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-11029498-8', 'php-internals.com');
-  ga('send', 'pageview');
-</script>
--- web/templates/layout/book.php
+++ web/templates/layout/book.php
@@ -11,6 +11,7 @@
 	<link rel="alternate" type="application/rss+xml" title="<?php echo SITE_NAME;?>" href="<?php echo url_for("/feed/"); ?>" />
 	<link rel="shortcut icon" href="<?php echo url_for("/favicon.ico"); ?>" type="image/vnd.microsoft.icon">
 	<!--[if IE]><?php echo stylesheet_include_tag('ie.css'); ?><![endif]-->
+	<?php SimpieView::include_partial("../templates/layout/_google_analytics.php"); ?>
 </head>
 <body id='book'>
 	<?php SimpieView::include_partial("../templates/layout/_header.php"); ?>
