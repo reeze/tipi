2f05cccc440bb5d0f009560a4d6a602f62e788e7
Reeze Xia <reeze@php.net>
2013-04-19 00:01:43 +0800

    Add tag for google

--- web/templates/layout/main.php
+++ web/templates/layout/main.php
@@ -9,6 +9,7 @@
 	<meta name="description" content="<?php echo SITE_DESC; ?>" />
 	<link rel="alternate" type="application/rss+xml" title="<?php echo SITE_NAME;?>" href="<?php echo url_for("/feed/"); ?>" />
 	<link rel="shortcut icon" href="<?php echo url_for("/favicon.ico"); ?>" type="image/vnd.microsoft.icon">
+	<meta name="google-site-verification" content="9MdZyBbYUlKcd9zfedQJEKhB66PngS1UUGHZ3NbZw70" />
 </head>
 <body id="home">
 	<?php SimpieView::include_partial("templates/layout/_header.php"); ?>
